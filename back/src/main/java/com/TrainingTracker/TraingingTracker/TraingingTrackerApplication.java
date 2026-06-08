51c0d21ce3873d41c3a853627595e14874e67690
mahmoud-1 <mahmood444e@gmail.com>
2026-06-09 02:41:17 +0300

    intialize the repo

---
+++ back/src/main/java/com/TrainingTracker/TraingingTracker/TraingingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.TrainingTracker.TraingingTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraingingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraingingTrackerApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/TrainingTracker/TraingingTracker/TraingingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrainingTracker.TraingingTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraingingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
